2df26790d1edfea4fca062537a5945b3bc61b572
Erik <erik@bigdon.de>
2021-11-06 16:22:48 +0100

    Hardcoded b&c, validation check if current gesture is in known "alphabet"

--- Basics.py
+++ Basics.py
@@ -4,6 +4,33 @@ import cv2
 import mediapipe as mp
 import time
 
+
+def checkForSimilarities(inputedGesture):
+    alphabet = [
+        [["C"], [0, 0], [31, -83], [38, -84], [40, -2], [46, -82], [50, -112], [54, -75], [54, -120], [62, -115], [66, -20], [68, -101], [71, -121], [78, -128], [83, -33], [83, -126], [84, -114], [90, -123], [98, -48], [99, -128], [100, -121], [101, -127]],
+        [["B"],[0, 0], [2, 97], [2, 25], [3, 53], [26, -28], [27, 216], [28, 83], [29, 30], [29, -2], [48, -45], [52, 78], [53, -16], [55, 21], [70, 91], [74, 194], [74, -30], [79, 82], [79, -2], [82, 30], [99, 109], [104, 149]]
+    ]
+    found = False
+    index = 0
+    try:
+        for letter in alphabet:
+            if found:
+                break
+            index = 1
+            for line in inputedGesture:
+                #print(f"1: {line[1]} and 2: {line[2]}")
+                if (letter[index])[0]-25 <= line[1] <= (letter[index])[0]+25:
+                    if (letter[index])[1]-25 <= line[2] <= (letter[index])[1]+25:
+                        if index == len(inputedGesture)-1:
+                            print((letter[0])[0], end="")
+                            found = True
+                else:
+                    break
+                index += 1
+    except:
+        print("", end="")
+
+
 def list_ports():
     dev_port = 0
     ports = []
@@ -45,6 +72,23 @@ def get_right_camera(list_of_drivers): #Zeige alle Bilder und frag ab ob richtig
                     break
 
 
+def quickSort(list):
+    if not list:
+        return list
+    pivot = list[0]
+    lesser = quickSort([x for x in list[1:] if x[1] < pivot[1]])
+    greater = quickSort([x for x in list[1:] if x[1] >= pivot[1]])
+    return lesser + [pivot] + greater
+
+
+def fixateList(list):
+    value1 = list[0][1]
+    value2 = list[0][2]
+    for indx, i in enumerate(list):
+        list[indx][1] = list[indx][1] - value1
+        list[indx][2] = list[indx][2] - value2
+    return list
+
 #cap = cv2.VideoCapture(get_right_camera(list_ports()))
 selectedDriver = list_ports()
 if str(selectedDriver) == "ERROR! Could not match input with available Driver":
@@ -58,8 +102,9 @@ mpDraw = mp.solutions.drawing_utils
 
 pTime = 0
 cTime = 0
-isRightCamera = None
 cv2.waitKey(1)
+
+handGesture = []
 
 while True:
     success, img = cap.read()
@@ -67,17 +112,25 @@ while True:
     results = hands.process(imgRGB)
     # print(results.multi_hand_landmarks)
 
+    toSort = []
+
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
             for id, lm in enumerate(handLms.landmark):
                 # print(id, lm)
                 h, w, c = img.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
-                print(id, cx, cy)
+                toSort.append([id,cx,cy])
+                #print(id, cx, cy)
                 # if id == 4:
                 cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
 
             mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+
+    if len(toSort)>0:
+        toSort = fixateList(quickSort(toSort))
+        handGesture.append(toSort)
+        checkForSimilarities(toSort)
 
     cTime = time.time()
     fps = 1 / (cTime - pTime)
@@ -89,6 +142,7 @@ while True:
     cv2.imshow("Image", img)
     if cv2.waitKey(1) & 0xFF == ord(' '):
         break
-        
+for line in handGesture:
+    print(line)
 cap.release()
 cv2.destroyAllWindows()
